38373da40fd638431917dce507697c618e5ffdb4
spriyanka87 <sajjanaupriyanka@gmail.com>
2025-09-14 17:20:24 +0530

    changes

---
+++ admin/Admin.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Adminscreen from './screens/Adminscreen'
+
+
+function Admin() {
+  return (
+    <div>
+      <Adminscreen/>
+    </div>
+  )
+}
+
+export default Admin
